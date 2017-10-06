f7ed2f96ae0dd2b6f8320a8e1fb16f9e1166677f
Balazs Prjevara <prjevarabalazs@gmail.com>
2017-10-06 12:53:31 +0200

    feat(candyshop): add skeleton

--- candyshop/candy_shop.py
+++ candyshop/candy_shop.py
@@ -17,6 +17,24 @@
 # The CandyShop should be represented as string in this format:
 # "Inventory: 3 candies, 2 lollipops, Income: 100, Sugar: 400gr"
 
+class CandyShop:
+
+    def __init__(self, sugar):
+        pass
+        
+    def create_sweets(self, candy):
+        pass
+
+    def sell(self, candy, amount):
+        pass
+
+    def raise_prices(self, amount):
+        pass
+
+    def buy_sugar(self, amount):
+        pass
+    
+            
 candy_shop = CandyShop(300)
 candy_shop.create_sweets("candy")
 candy_shop.create_sweets("candy")
@@ -26,7 +44,7 @@ print(candy_shop)
 # Should print out:
 # Invetory: 2 candies, 2 lollipops, Income: 0, Sugar: 270gr
 candy_shop.sell("candy", 1)
-print(candy_shop
+print(candy_shop)
 # Should print out:
 # "Invetory: 1 candies, 2 lollipops, Income:20, Sugar: 285gr"
 candy_shop.raise_prices(5)
