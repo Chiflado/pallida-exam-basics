1ab92eed15779c5a3fdd7ff03d532f9ab30888da
Balazs Prjevara <prjevarabalazs@gmail.com>
2017-10-06 09:40:27 +0200

    feat(unique_chars): add test for find unique letters

--- uniquechars/test_for_uniqchars.py
+++ uniquechars/test_for_uniqchars.py
@@ -6,6 +6,9 @@ class TestUnique_chars(unittest.TestCase):
     def test_empty_string(self):
         self.assertEqual(unique_chars.unique_characters(''), [])
 
+    def test_removing_same_letters(self):
+        self.assertEqual(unique_chars.unique_characters("anagram"), ["n", "g", "r", "m"])
+
 
 if __name__ == '__main__':
     unittest.main()
